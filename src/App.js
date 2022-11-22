97ce066276b8a5e7b482b8aadcedcd56056e011d
Ioana Dana <ioanadananitescu@gmail.com>
2022-11-22 16:05:06 +0200

    version 1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn AWS and Amplify, actually. With React as framework. 
         </a>
       </header>
     </div>
